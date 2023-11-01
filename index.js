e13d2619d557c7af8c05699f0e3a996f00c13e45
Soliu Adeosun <sawlew@gmail.com>
2023-11-02 00:09:42 +0100

    UI completed

--- index.js
+++ index.js
@@ -46,16 +46,16 @@ const fetchTodos = () => {
     const todo_db = JSON.parse(localStorage.getItem(DB_NAME)) || [];
     const noTodo = todo_db.length === 0;
     if(noTodo){
-      todoBox.innerHTML = `<p class="text-center pt-5 text-gray-300">Your todos will appear here</p>`;
+      todoBox.innerHTML = `<p class="text-center pt-5 text-blue-300">Your todos will appear here</p>`;
       return;
     }
 
     const todos = todo_db.sort((a, b) => a.created_at < b.created_at ? 1 : a.created_at > b.created_at ? -1 : 0
     ).map((todo) => {
         return `
-        <div class="group flex justify-between p-3 mb-5 rounded bg-gray-100 hover:bg-gray-200">
-      <p>${todo.title}</p>
-      <div class="flex w-[12%] justify-between hidden group-hover:flex">
+        <div class="group flex justify-between p-3 mb-5 rounded bg-blue-100 hover:bg-blue-200">
+      <p class="overflow-ellipsis overflow-hidden">${todo.title}</p>
+      <div class="flex w-[12%] justify-between hidden group-hover:flex cursor-pointer">
         <svg xmlns="http://www.w3.org/2000/svg" color="blue" onclick="handleEditMode('${todo.id}')" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
         </svg>
